1dd6dd80417f78d0dfe0a7355fa8e3e3c6d0b289
Yngrid Coello <yngrdyn@gmail.com>
2019-09-27 18:11:37 +0200

    adding force graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ɵɵNgOnChangesFeature, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'orgChart';
+
+export class AppComponent implements OnInit {
+
+  data$ = this.http.get<any>('./assets/data/data.json');
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {}
+
 }
